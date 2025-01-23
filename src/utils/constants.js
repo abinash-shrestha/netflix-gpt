50c78b51b58970aeadcb70a0b1beca51d2d725f5
Abinash <154217568+abinash-shrestha@users.noreply.github.com>
2025-01-23 23:54:37 +0545

    integrated TMDB API's

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,11 @@ export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_produ
 export const USER_AVATAR = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
 
 export const BACKGROUND_IMAGE = "https://assets.nflxext.com/ffe/siteui/vlv3/150c4b42-11f6-4576-a00f-c631308b1e43/web/NP-en-20241216-TRIFECTA-perspective_76746de6-1d78-441b-bf03-f98deb75294b_large.jpg"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: process.env.REACT_APP_TMDB_API_KEY
+    }
+};
