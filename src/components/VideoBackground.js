53dff4df8d2ee91ea8735510841f67d847fe4719
Abinash <154217568+abinash-shrestha@users.noreply.github.com>
2025-01-28 22:35:21 +0545

    fetched the trailer information of a first movie form now playing movie list

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { API_OPTIONS } from '../utils/constants'
 
-const VideoBackground = () => {
+const VideoBackground = ({ id }) => {
+    const getMovieTrailer = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/939243/videos?language=en-US', API_OPTIONS)
+        const json = await data.json();
+        console.log(json)
+
+        const filteredData = json.results.filter((video) => video.type === "Trailer");
+        const trailerData = filteredData.length ? filteredData[0] : json.results[0];
+        console.log(trailerData);
+
+
+
+    }
+    useEffect(() => {
+        getMovieTrailer();
+    }, [])
     return (
         <div>
             VideoBackground
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -3,11 +3,11 @@ import React from 'react'
 const VideoTitle = ({ title, overview }) => {
     return (
         <div className='pt-20 pl-10'>
-            <h1>{title}</h1>
-            <p>{overview}</p>
-            <div>
-                <button>Play</button>
-                <button>More Info</button>
+            <h1 className='text-2xl font-bold'>{title}</h1>
+            <p className='w-1/3 text-base'>{overview}</p>
+            <div className=''>
+                <button className='px-10 py-3 m-2 text-xl text-white bg-gray-500 bg-opacity-50 rounded-lg'>Play</button>
+                <button className='px-10 py-3 m-2 text-xl text-white bg-gray-500 rounded-lg'>More Info</button>
 
             </div>
 
