9afd49a0d62386109123662c48e186bdaefe23aa
abinash-skillprompt <abinash.skillprompt@gmail.com>
2025-02-03 17:21:42 +0545

    fixed import error

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,7 @@
 import Header from './Header';
 import MainContainer from './MainContainer';
 import SecondaryContainer from './SecondaryContainer';
-import useNowPlayingMovies from '../hooks/usePopularMovies';
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
 
 const Browse = () => {
   useNowPlayingMovies();
