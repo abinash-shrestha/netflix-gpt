0474846a367837add848b825decad391d33238d8
abinash-skillprompt <abinash.skillprompt@gmail.com>
2025-02-04 23:37:54 +0545

    fetched upcoming, top rated and popular movies through custom hooks and displayed in secondary container and created reducers and dispatched each one of them

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import MovieList from './MovieList';
 
 const SecondaryContainer = () => {
-    const movies = useSelector((store) => store.movies)
-    return movies && (
-        <div className='bg-black z-20'>
-            <div className='-mt-80 ml-12  relative z-20'>
-
-                <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
-                <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
-                <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
-                <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
-            </div>
-
-
+  const movies = useSelector((store) => store.movies);
+  return (
+    movies && (
+      <div className="bg-black z-20">
+        <div className="-mt-80 ml-12  relative z-20">
+          <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+          <MovieList title="Popular Movies" movies={movies.popularMovies} />
+          <MovieList title="Top Rated Movies" movies={movies.topRatedMovies} />
+          <MovieList title="Upcoming Movies" movies={movies.upComingMovies} />
         </div>
+      </div>
     )
-}
+  );
+};
 
 export default SecondaryContainer;
